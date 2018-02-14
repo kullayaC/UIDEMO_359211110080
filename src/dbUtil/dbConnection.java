496a6937f9ce3fe033327705e5b299bc5d71a412
kullayaC <clear1_1@hotmail.com>
2018-02-14 11:10:43 +0700

    Connect to DB

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -5,7 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConnection {
-    private static  final String SQCONN="jdbc:school.sqlite";
+    private static  final String SQCONN="jdbc:sqlite:school.sqlite";
+
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -15,5 +16,5 @@ public class dbConnection {
         }
         return null;
     }//getconnection
-    
+
 }//class
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,33 @@
 package sample;
 
-public class Controller {
-}
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    private  loginModel model = new loginModel();
+    //import object from scence builder
+    @FXML
+    private Label dbStatus;
+    @FXML
+    private Label loginStatus;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button btnLogin;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if (this.model.isDatabaseConnection()){
+            this.dbStatus.setText("Connectec to DB");
+        }else {
+            this.dbStatus.setText("Not Connect to DB");
+        }
+
+    }
+}//class
